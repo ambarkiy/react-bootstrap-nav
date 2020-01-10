acfd0e4c069eb6c096160e0685230da967897e47
ambarkiy <ambarki.youssef@gmail.com>
2020-01-10 11:50:32 +0100

    first commit

--- src/App.test.js
+++ src/App.test.js
@@ -2,8 +2,8 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test('render First Test p element', () => {
+	const { getByText } = render(<App />);
+	const linkElement = getByText(/First Test/i);
+	expect(linkElement).toBeInTheDocument();
 });
